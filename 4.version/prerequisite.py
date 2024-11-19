fe4824f29684b4d633931417a20c8181a8e37e31
lhgiang040504 <lhgiang040504@gmail.com>
2024-11-20 06:40:45 +0700

    compatible spacy with en_core_web_sm

--- 4.version/prerequisite.py
+++ 4.version/prerequisite.py
@@ -2,7 +2,7 @@ import os
 import nltk
 
 # Install SpaCy Dependencies
-os.system('python -m pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz')
+os.system('python -m pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.6.0/en_core_web_sm-3.6.0-py3-none-any.whl')
 
 # Install nltk Dependencies
 nltk.download('maxent_ne_chunker')
